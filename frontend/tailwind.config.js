9f2621da3a8276694c76027a6d1d7db74fe7dd8c
eyurenalison <eeyuren1@gmail.com>
2025-04-10 23:45:27 +0100

    feat:adding workflows, laravel jetstream and basic setup for project

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -3,6 +3,7 @@ module.exports = {
   content: [
     "./frontend/**/*.html", // if using plain HTML
     "./frontend/**/*.js", // if your JS has Tailwind classes
+    "./index.html",
   ],
   theme: {
     extend: {},
