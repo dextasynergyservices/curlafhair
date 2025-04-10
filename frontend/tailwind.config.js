1b908f6fea5d1ebe63e4ee8b56e2f33588ae2aa2
eyurenalison <eeyuren1@gmail.com>
2025-04-10 01:29:55 +0100

    initial commit to setup the project

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./frontend/**/*.html", // if using plain HTML
+    "./frontend/**/*.js", // if your JS has Tailwind classes
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
